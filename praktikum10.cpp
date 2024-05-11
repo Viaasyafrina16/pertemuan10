99a1e47581cb27c1646ccaca6251387518ebecab
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-11 10:43:21 +0700

    menambahkan switch

--- praktikum10.cpp
+++ praktikum10.cpp
@@ -72,7 +72,16 @@ int main() {
     int value;
 
     while (choice != 5) {
-        
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cin >> choice;
+
+        switch (choice) {
+            
+        }
+
     }
 
 }
