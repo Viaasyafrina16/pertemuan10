81328861578c4e5a89263737612afe42d19069a7
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-11 10:19:06 +0700

    membuat prosedur void peek

--- praktikum10.cpp
+++ praktikum10.cpp
@@ -35,8 +35,19 @@ public:
 
     // Pop operation: Remove the topmost element from the stack
     void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty." << endl;
+        }
+        cout << "Popped value: " << top->data << endl;
+        top = top->next; // Update the top pointer to the next node
+
+    }
+
+    // peek/Top operation: Retrieve the value of the topmost element without removing it
+    void peek() {
         
     }
+    
 
     
 }
