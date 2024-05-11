b3a2dfd1dbee8c528c9e96a76666582b74639195
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-11 09:43:15 +0700

    membuat akses modifier yang bersifat private pada class stack

--- praktikum10.cpp
+++ praktikum10.cpp
@@ -5,5 +5,16 @@ using namespace std;
 // Node class representing a single node in the linked list
 class Node {
 public :
-    
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
+};
+
+// stack class
+class stack {
+private:
+    Node* top; 
 }
