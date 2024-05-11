606372bd3269dc293b2422b455a068b4d8d61153
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-11 09:47:08 +0700

    menambahkan int push

--- praktikum10.cpp
+++ praktikum10.cpp
@@ -19,4 +19,12 @@ private:
     Node* top; // Pointer to the top node of the stack
 
 public:
+    stack() {
+        top = NULL; //Initialize the stack with a null top pointer
+    }
+
+    // Push operation: insert an element onto the top of the stack
+    int push(int value) {
+        
+    }
 }
