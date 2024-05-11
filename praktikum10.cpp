6da3ec6f08122d7fba7602ed4ed78cf30487f2d7
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-11 10:26:34 +0700

    membuat bool isEmpty

--- praktikum10.cpp
+++ praktikum10.cpp
@@ -45,7 +45,26 @@ public:
 
     // peek/Top operation: Retrieve the value of the topmost element without removing it
     void peek() {
+        if (top == NULL) {
+            cout << "List is empty." << endl;
+            
+        }
+        else {
+            Node* current= top;
+            while (current != NULL) {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+             cout << endl;
+        } // Return the value of the top node
+    }
+
+    // isEmpty operation: Check if the stack is empty
+    bool isEmpty() {
         
+    }
+
+
     }
     
 
